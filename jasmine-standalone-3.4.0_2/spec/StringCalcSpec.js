4aaf27490585daffb002bfb893a07e571fc75496
MihlaliNelana <mihlali.nelana@umuzi.org>
2019-06-18 10:37:28 +0200

    cater for different delimiters

--- jasmine-standalone-3.4.0_2/spec/StringCalcSpec.js
+++ jasmine-standalone-3.4.0_2/spec/StringCalcSpec.js
@@ -3,54 +3,51 @@ describe("String Calculator",  function(){
     calculator = new StringCalculator();
   });
 
-  it("should be able to return sum of two numbers that are split by a comma", function(){
-    expect(calculator.add("1,2")).toEqual(3);
+  it("should be able to return sum of two or more numbers that are split by a comma", function(){
+    expect(calculator.add("1,2,1")).toEqual(4);
   });
 
-  it("should be able to handle unknown amount of numbers", function(){
-    // creates a random array
+  it("should allow the Add method to handle an unknown amount of numbers", function(){
+    // creates a randomized array
     randomArray = (length, max) => [new Array(length)]
     .map(() => Math.round(Math.random() * max));
-    //randomly creates a random number between 1 and 100
+    //randomly create a random number form 1 - 100
     randomNum = Math.floor((Math.random() * 100) + 1);
-    res = randomArray(randomNum,randomNum);
+    respose = randomArray(randomNum,randomNum);
     // sum of array
-    sum = res.reduce((pv, cv) => pv+cv, 0);
+    sum = respose.reduce((i, j) => i+j, 0);
     // convert the array to a string
-    arg = res.join();
-    //expect arguments passed to be sum
+    arg = respose.join();
+    //expect arguments passed toEqual sum
     expect(calculator.add(arg)).toEqual(sum);
   });
 
-  it("should allow \n in between the input number string", function(){
-    expect(calculator.add("1\n2,3")).toEqual(6);
+  it("should allow \n in between the input number", function(){
+    expect(calculator.add("1\n2\n3")).toEqual(6);
   });
 
-  xit("should throw an error if number is '1,\n'", function(){
-    expect(calculator.add("1,\n")).toThrow(new Error("not allowes"));
-  })
-
-  it("should return 0 if input is an empty string", function () {
-      expect(calculator.add("")).toBe(0);
+  it("should support different delimeters", () => {
+    expect(calculator.add("//;\n2;2")).toEqual(4);
   });
-  
-  xit("Numbers bigger than 1000 should be ignored, so adding 2 + 1001 = 2", function(){
-    expect(calculator.add("1001 + 2")).toBe(2);
-  
-  it("should return the number when a single number is passed", function(){
-    expect(calculator.add('1')).toEqual(1);
-  });
-
-  it("should return 2 if input is \"1\\n0,1\"", function () {
-      expect(calculator.add("1\n0,1")).toBe(2);
-    });
 
   it("should deny negative numbers", function(){
     expect( function(){ calculator.add("-1,2,3");}).toThrow(new Error("Negative numbers are not allowed"));
   });
 
-  it("should return 6 if input is \"1,1,1,1,1,1\"", function () {
-      expect(calculator.add("1,1,1,1,1,1")).toBe(11);
+  it("should return 0 if input is an empty string", function () {
+      expect(calculator.add("")).toBe(0);
+  });
+  
+  it("should return the number when a single number is passed", function(){
+    expect(calculator.add('1')).toEqual(1);
+  });
+
+  it("should return 2 if input is \"1\n0,1\"", function () {
+      expect(calculator.add("1\n0,1")).toBe(2);
     });
-  })
-});
+
+  it("should return 6 if input is \"1,1,1,1,1,1\"", function () {
+      expect(calculator.add("1,1,1,1,1,1")).toBe(6);
+    });
+
+  });
--- jasmine-standalone-3.4.0_2/src/StringCalc.js
+++ jasmine-standalone-3.4.0_2/src/StringCalc.js
@@ -9,21 +9,35 @@ function StringCalculator(){
           }
           
           // replace comma
-          numbers = this.number.replace(/(\;|\]|\%|\*|\/|\[|\r\n|\n|\r)/gm, ",");
-          if (numbers == '') {  
+          numbers = this.number.replace(/(\;|\]|\%|\*|\/|\[|\r\n|\n|\r)/gm, ",");   //support different delimiter
+          if (numbers == ''){      //return 0 if string is emoty
               return 0;
-          } else if(numbers.length === 1) {   
-              return (parseInt(numbers));  
-          } else 
-              {
-                  res = numbers.split(',');
-                  var total = 0;
-                  var arrayLength = res.length; 
-                  
-                  for (var i = 0; i < res.length; i++) {  
-                      total = total + parseInt(res[i]);  
-                  }
+          } 
+          else if(numbers.length === 1) {   
+            return (parseInt(numbers));  
+          } 
+          else 
+            {
+                response = numbers.split(',');    //split commas
+                var total = 0;
+                var arrayLength = response.length; 
+            //console.log(response);        
+          
+            for(var j = 0; j < arrayLength; j++){
+                if(response[j] == ""){
+                    response.splice(j,1);
+                    j--
+                }    
+            }
+           //console.log(response);
+          
+           for (var i = 0; i < response.length; i++) {  
+                  total = total + parseInt(response[i]);
+                }
               return total;   
           }   
  }
 };
+
+// let calculator = new StringCalculator();
+// calculator.add("//;\n2;2");
